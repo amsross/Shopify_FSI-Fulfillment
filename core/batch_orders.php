27f6f582b5d66d826a2b0e286df3edfa656c4392
Matt Ross <matt@rhinorojo.com>
2013-08-02 01:03:43 -0500

    Fewer local DB calls, less info in the db

--- core/batch_orders.php
+++ core/batch_orders.php
@@ -1,6 +1,7 @@
 <?php
 
 	$batched_orders = array();
+	$batchedOrders = array();
 	$new_orders = array();
 
 	try {
@@ -36,6 +37,7 @@
 			if (count($resultSelect->num_rows) > 0) {
 
 				while ($row = $resultSelect->fetch_assoc()) {
+
 					$preferences = $row;
 				}
 			}
@@ -52,27 +54,51 @@
 		if ($resultSelect = $mysqli->query($select)) {
 
 			while ($row = $resultSelect->fetch_assoc()) {
+
 				$batched_orders[] = $row;
 			}
 
 			$resultSelect->close();
 		}
 
-		// Get all paid orders to see which have been batched.
-		// Store the others.
-		$order_object_response = $shopifyClient->call('GET', '/admin/orders.json?financial_status=paid');
-		$smarty->assign('order_object', $order_object_response);
-		
-		$order_objects = json_encode( $order_object_response );
-		$order_objects = json_decode( $order_objects );
+		// Determine the CSV's filename
+		$fileCSVName = 'BRYord' . date('mdY') . '.csv';
 
-		foreach ( $order_objects as $order_object ) :
+		// Try to get an existing version of the file on the server
+		if (@ftp_get($ftp_conn, $fileCSVName, FTP_SERVER_DIR . $fileCSVName, FTP_BINARY) || file_exists($fileCSVName)) {
+			$line = "";
+		} else {
+			// If no previous version exists, create the column headers
+			$line = "FileType,ClientCode,OrderNumber,CarrierCode,ShipToName,ShipToAddr1,ShipToAddr2,ShipToAddr3,ShipToAddr4,ShipToCity,ShipToState,ShipToZip,ShipToCountry,ItemNumber,QtyOrdered,ShipToPhone,EmailName,EmailAddress,OrderDate\n";
+		}
+
+		// Open the CSV file for writing without destroying existing content
+		$fileCSV = fopen($fileCSVName, 'a');
+
+		if (!isset($_POST['orders']) || empty($_POST['orders'])) :
+
+			throw new Exception("Error: No orders selected.");
+		endif;
+
+		$order_object_responses = array();
+
+		foreach ($_POST['orders'] as $order) :
+
+			// Get the selected order
+			// Store the others.
+			$order_object_response = $shopifyClient->call('GET', "/admin/orders/{$order}.json");
+
+			$order_object_responses[] = $order_object_response;
+			
+			$order_objects = json_encode( $order_object_response );
+			$order_object = json_decode( $order_objects );
 
 			// Make sure this order was selected for batching
 			if ( !is_array($_POST['orders']) || !in_array($order_object->id, $_POST['orders']) ) :
 
 				continue;
 			else :
+			
 				foreach ( $batched_orders as $batched_order ) :
 
 					if ($order_object->id == $batched_order['OrderNumber']) {
@@ -110,133 +136,113 @@
 					false
 				)
 				";
+
+			// Create the DB record for the order to poll for status later
+			$insert = "INSERT INTO
+				orders (
+					Token, OrderNumber, Batched
+				)
+				VALUES (
+					'{$_SESSION['token']}',
+					'{$order_object->id}',
+					false
+				)
+				";
+
 			if (!$resultInsert = $mysqli->query($insert)) :
+
+				throw new Exception("Error: " . $mysqli->error);
+			endif;
+
+			// Add thisorder to the list to mark as batched if we don't run into trouble
+			$batchedOrders[] = $order_object;
+
+			foreach ( $order_object->line_items as $line_item ) :
+
+				// Write the order's info as a line in the CSV file
+					// FileType
+				$line .= "STANDARD,";
+					// ClientCode
+				$line .= $preferences['ClientCode'] . ",";
+					// OrderNumber
+				$line .= $order_object->id . ",";
+					// CarrierCode
+				$line .= $preferences['CarrierCode'] . ",";
+					// ShipToName
+				$line .= $order_object->shipping_address->first_name . " " . $order_object->shipping_address->last_name . ",";
+					// ShipToAddr1
+				$line .= $order_object->shipping_address->address1 . ",";
+					// ShipToAddr2
+				$line .= $order_object->shipping_address->address2 . ",";
+					// ShipToAddr3
+				$line .= ",";
+					// ShipToAddr4
+				$line .= ",";
+					// ShipToCity
+				$line .= $order_object->shipping_address->city . ",";
+					// ShipToState
+				$line .= $order_object->shipping_address->province_code . ",";
+					// ShipToZip
+				$line .= $order_object->shipping_address->zip . ",";
+					// ShipToCountry
+				$line .= $order_object->shipping_address->country_code . ",";
+					// ItemNumber
+				$line .= $line_item->sku . ",";
+					// QtyOrdered
+				$line .= $line_item->quantity . ",";
+					// ShipToPhone
+				$line .= $order_object->shipping_address->phone . ",";
+					// EmailName
+				$line .= $order_object->customer->first_name . " " . $order_object->customer->last_name . ",";
+					// EmailAddress
+				$line .= $order_object->customer->email . ",";
+					// OrderDate
+				$line .= $order_object->created_at . ",";
+				$line .= "\n";
+			endforeach;
+		endforeach;
+
+		// Let there be light!
+		fwrite($fileCSV, $line);
+
+		fclose($fileCSV);
+
+		// Try to upload CSV file
+		if (!@ftp_put($ftp_conn, FTP_SERVER_DIR . $fileCSVName, $fileCSVName, FTP_BINARY)) :
+
+			throw new Exception("Error: There was a problem while uploading $fileCSVName");
+		endif;
+
+		$smarty->assign('response', count($batchedOrders) . ' Orders Batched');
+		
+		// Mark all the batched products as such
+		foreach ( $batchedOrders as $batchedOrder ) :
+
+			$update = "UPDATE orders
+				SET Batched = true
+				WHERE OrderNumber = '{$batchedOrder->id}'
+				AND Token = '{$_SESSION['token']}'";
+
+			if (!$resultUpdate = $mysqli->query($update)) :
+
+				// Save a version of the CSV for later investigation
+				if ( file_exists( $fileCSVName ) ) :
+
+					rename( $fileCSVName, 'error-'.$fileCSVName );
+				endif;
 
 				throw new Exception("Error: " . $mysqli->error);
 			else :
 
-				foreach ( $order_object->line_items as $line_item ) :
-
-					// Save the items in the DB too
-					// We'll need these for the CSV
-					$query = "INSERT INTO
-								items (Token, OrderNumber, ItemNumber, QtyOrdered )
-							VALUES (
-								'{$_SESSION['token']}',
-								'{$order_object->id}',
-								'{$line_item->sku}',
-								'{$line_item->quantity}'
-							)
-						";
-
-					if (!$mysqli->query($query)) {
-
-						throw new Exception("Error: " . $mysqli->error);
-					}
-				endforeach;
+				if ( file_exists( $fileCSVName ) ) :
+				
+					// Get rid of the CSV file
+					unlink( $fileCSVName );
+				endif;
 			endif;
 		endforeach;
-		
-		// Get all the stored items to build the CSV batch file with
-		$query = "SELECT * FROM items
-			JOIN orders on items.OrderNumber = orders.OrderNumber
-			AND orders.Batched = false
-			WHERE orders.Token = '{$_SESSION['token']}'
-		";
 
-		if (!$resultSelect = $mysqli->query($query)) :
-
-			throw new Exception("Error: " . $mysqli->error);
-		else :
-
-			if ( $resultSelect->num_rows > 0 ) :
-		
-				// Determine the CSV's filename
-				$fileCSVName = 'BRYord' . date('mdY') . '.csv';
-
-				// Try to get an existing version of the file on the server
-				if ( @ftp_get($ftp_conn, $fileCSVName, FTP_SERVER_DIR . $fileCSVName, FTP_BINARY) ) {
-					$line = "";
-				} else {
-					// If no previous version exists, create the column headers
-					$line = "FileType,ClientCode,OrderNumber,CarrierCode,ShipToName,ShipToAddr1,ShipToAddr2,ShipToAddr3,ShipToAddr4,ShipToCity,ShipToState,ShipToZip,ShipToCountry,ItemNumber,QtyOrdered,ShipToPhone,EmailName,EmailAddress,OrderDate\n";
-				}
-
-				// Open the CSV file for writing without destroying existing content
-				$fileCSV = fopen($fileCSVName, 'a');
-
-				// Let there be light!
-				fwrite($fileCSV, $line);
-				
-				while ( $order_item = $resultSelect->fetch_object() ) :
-
-					// Add thisorder to the list to mark as batched if we don't run into trouble
-					$batchedOrders[] = $order_item;
-
-					// Write the order's info as a line in the CSV file
-					$line = $order_item->FileType . ",";
-					$line .= $order_item->ClientCode . ",";
-					$line .= $order_item->OrderNumber . ",";
-					$line .= $order_item->CarrierCode . ",";
-					$line .= $order_item->ShipToName . ",";
-					$line .= $order_item->ShipToAddr1 . ",";
-					$line .= $order_item->ShipToAddr2 . ",";
-					$line .= $order_item->ShipToAddr3 . ",";
-					$line .= $order_item->ShipToAddr4 . ",";
-					$line .= $order_item->ShipToCity . ",";
-					$line .= $order_item->ShipToState . ",";
-					$line .= $order_item->ShipToZip . ",";
-					$line .= $order_item->ShipToCountry . ",";
-					$line .= $order_item->ItemNumber . ",";
-					$line .= $order_item->QtyOrdered . ",";
-					$line .= $order_item->ShipToPhone . ",";
-					$line .= $order_item->EmailName . ",";
-					$line .= $order_item->EmailAddress . ",";
-					$line .= $order_item->OrderDate . "\n";
-
-					fwrite($fileCSV, $line);
-				endwhile;
-
-				fclose($fileCSV);
-
-				// Try to upload CSV file
-				if (!@ftp_put($ftp_conn, FTP_SERVER_DIR . $fileCSVName, $fileCSVName, FTP_BINARY)) :
-
-					throw new Exception("Error: There was a problem while uploading $fileCSVName");
-				endif;
-
-				$smarty->assign('response', $resultSelect->num_rows . ' Orders Batched');
-				
-				// Mark all the batched products as such
-				foreach ( $batchedOrders as $batchedOrder ) :
-
-					$update = "UPDATE orders
-						SET Batched = true
-						WHERE OrderNumber = '{$batchedOrder->OrderNumber}'
-						AND Token = '{$_SESSION['token']}'";
-
-					if (!$resultUpdate = $mysqli->query($update)) :
-
-						// Save a version of the CSV for later investigation
-						if ( file_exists( $fileCSVName ) ) :
-
-							rename( $fileCSVName, 'error-'.$fileCSVName );
-						endif;
-
-						throw new Exception("Error: " . $mysqli->error);
-					else :
-
-						if ( file_exists( $fileCSVName ) ) :
-						
-							// Get rid of the CSV file
-							unlink( $fileCSVName );
-						endif;
-					endif;
-				endforeach;
-
-				$smarty->assign('batched_orders', $batchedOrders);
-			endif;
-		endif;
+		$smarty->assign('batched_orders', $batchedOrders);
 
 		$mysqli->close();
 
--- templates/batch_orders.tpl
+++ templates/batch_orders.tpl
@@ -5,17 +5,15 @@
 <table style="width:100%;">
 	<tbody>
 		<tr>
-			<th style="width:25%;">Order Number</th>
-			<th style="width:25%;">Ship To Name</th>
-			<th style="width:25%;">Order Date</th>
-			<th style="width:25%;">Batched Date</th>
+			<th style="width:33%;">Order Number</th>
+			<th style="width:33%;">Ship To Name</th>
+			<th style="width:33%;">Order Date</th>
 		</tr>
 		{foreach $batched_orders as $batched_order}
 		<tr>
-			<td><a href="https://{$shopifyClient->shop_domain}/admin/orders/{$batched_order->OrderNumber}" target="_blank">{$batched_order->OrderNumber}</a></td>
-			<td>{$batched_order->ShipToName}</td>
-			<td>{$batched_order->OrderDate}</td>
-			<td>{$batched_order->ScrapedDate}</td>
+			<td><a href="https://{$shopifyClient->shop_domain}/admin/orders/{$batched_order->id}" target="_blank">{$batched_order->id}</a></td>
+			<td>{$batched_order->shipping_address->first_name} {$batched_order->shipping_address->last_name}</td>
+			<td>{$batched_order->created_at}</td>
 		</tr>
 		{/foreach}
 	</tbody>
