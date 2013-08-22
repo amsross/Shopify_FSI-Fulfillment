fa1a1a98b0368549726a6427dae48675d8d1f924
Matt Ross <matt@rhinorojo.com>
2013-08-22 15:08:47 -0400

    Preferences typo fix

--- core/batch_orders.php
+++ core/batch_orders.php
@@ -71,7 +71,7 @@
 		$fileCSVName = $preferences['ClientCode'] . 'ord' . date('mdY') . '.csv';
 
 		// Try to get an existing version of the file on the server
-		if (@ftp_get($ftp_conn, $fileCSVName, $preferenes['FTPServerDir'] . $fileCSVName, FTP_BINARY) || file_exists($fileCSVName)) {
+		if (@ftp_get($ftp_conn, $fileCSVName, $preferences['FTPServerDir'] . $fileCSVName, FTP_BINARY) || file_exists($fileCSVName)) {
 			$line = "";
 		} else {
 			// If no previous version exists, create the column headers
@@ -214,7 +214,7 @@
 		fclose($fileCSV);
 
 		// Try to upload CSV file
-		if (!@ftp_put($ftp_conn, $preferenes['FTPServerDir'] . $fileCSVName, $fileCSVName, FTP_BINARY)) :
+		if (!@ftp_put($ftp_conn, $preferences['FTPServerDir'] . $fileCSVName, $fileCSVName, FTP_BINARY)) :
 
 			throw new Exception("Error: There was a problem while uploading $fileCSVName");
 		endif;
